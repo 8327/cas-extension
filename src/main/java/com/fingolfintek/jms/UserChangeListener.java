b1979866fa9b4b06a666fe29155166ea139b5d1a
Milos <fingolfintek@gmail.com>
2013-10-30 20:02:01 +0100

    Added Jackson dependency, created user repository, added additional Spring configuration, refactored the code a bit

--- src/main/java/com/fingolfintek/jms/UserChangeListener.java
+++ src/main/java/com/fingolfintek/jms/UserChangeListener.java
@@ -1,19 +1,37 @@
 package com.fingolfintek.jms;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fingolfintek.dao.UserAuthenticationInfoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 @Component("userChangeListener")
 public class UserChangeListener implements MessageListener {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private @Autowired UserAuthenticationInfoRepository repository;
 
     @Override
     public void onMessage(Message message) {
-        logger.info(message.toString());
+        Assert.isInstanceOf(TextMessage.class, message, "Only text messages are allowed");
+
+        try {
+            TextMessage textMessage = (TextMessage) message;
+            logger.info(textMessage.getText());
+
+        } catch (JMSException e) {
+            logger.error("Encountered unexpected JMS exception");
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
