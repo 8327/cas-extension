dcd4beb47de79ec950b366f3616daa1f27a532e5
Milos <fingolfintek@gmail.com>
2013-10-30 22:35:21 +0100

    Moved project data into separate directory

--- src/main/java/com/fingolfintek/dao/UserAuthenticationInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fingolfintek.dao;
-
-import com.fingolfintek.model.UserAuthenticationInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAuthenticationInfoRepository extends JpaRepository<UserAuthenticationInfo, Long> {
-
-    UserAuthenticationInfo findByUsername(String username);
-}
